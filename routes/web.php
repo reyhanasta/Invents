f81aac563aad87e4a0c0d8b0eb8009030315518b
ramalisujimat <053155717@ecampus.ut.ac.id>
2025-12-15 16:08:20 +0700

    Add Asset model, controller, factory, and migration for asset management

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Category Routes
     Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
-    Route::get('categories/create', [\App\Http\Controllers\CategoryController::class, 'create'])->name('categories-create');
-    Route::get('categories/{category}/show', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories-show');
     Route::post('categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories-store');
-    Route::get('categories/{category}/edit', [\App\Http\Controllers\CategoryController::class, 'edit'])->name('categories-edit');
     Route::put('categories/{category}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories-update');
     Route::delete('categories/{category}', [\App\Http\Controllers\CategoryController::class, 'delete'])->name('categories-delete');
+
+    // Asset Routes
 });
 
 require __DIR__.'/settings.php';
