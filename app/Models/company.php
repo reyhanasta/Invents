768f4dc8d4c3134e76597c31ea988baddf7c1850
reyhanasta <astareyhan@gmail.com>
2025-12-29 16:03:43 +0700

    adding company

---
+++ app/Models/company.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class company extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanyFactory> */
+    use HasFactory;
+
+    protected guarded = ['id']
+}
