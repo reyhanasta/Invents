6da37aef0f43f988576567dc5c516920e9a4674b
reyhanasta <astareyhan@gmail.com>
2026-04-16 12:12:55 +0700

    feat: add AppLogoIcon component and project logo assets

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -5,7 +5,7 @@ export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
         <svg {...props} viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
             <image
                 // href="/project_logo/favicon-96x96.png"
-                href="/project_logo/icon-no-bg.png"
+                href="/project_logo/logo_kubr.png"
                 width="96"
                 height="96"
             />
