5e0d6176c15189307ec636f35d59c2e66abf5e0c
reyhanasta <astareyhan@gmail.com>
2026-04-16 16:39:46 +0700

    feat: configure CORS settings and initialize database seeders for core application modules

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            // CompanySeeder::class,
+            CompanySeeder::class,
             CategorySeeder::class,
             // LocationSeeder::class,
             // AssetSeeder::class,
